45ea2d1c86def5b9c4538f10bcea0ae317949a2b
lazyguyy <shelton.kueken@gmail.com>
2017-03-04 00:04:48 +0100

    replaced tabs with spaces

--- graphgui/GraphFrame.java
+++ graphgui/GraphFrame.java
@@ -5,22 +5,21 @@ import java.awt.event.*;
 import java.awt.*;
 import javax.swing.*;
 
-public class GraphFrame extends JFrame{
-	public GraphFrame(Graph g) {
-		super("Graphics");
-		setBackground(Color.white);
-		PainterPanel p = new PainterPanel(g, getBackground());
-		add(p);
-		pack();
-		addWindowListener(new WindowAdapter() {
-			@Override
-			public void windowClosing(WindowEvent w) {
-				p.stop();
-				dispose();
-			}
-		});
-		setLocationRelativeTo(null);
-		setVisible(true);
-	}
-
+public class GraphFrame extends JFrame {
+    public GraphFrame(Graph g) {
+        super("Graphics");
+        setBackground(Color.white);
+        PainterPanel p = new PainterPanel(g, getBackground());
+        add(p);
+        pack();
+        addWindowListener(new WindowAdapter() {
+            @Override
+            public void windowClosing(WindowEvent w) {
+                p.stop();
+                dispose();
+            }
+        });
+        setLocationRelativeTo(null);
+        setVisible(true);
+    }
 }
--- graphgui/GraphicsHelper.java
+++ graphgui/GraphicsHelper.java
@@ -3,18 +3,17 @@ package graphgui;
 import java.awt.*;
 
 public class GraphicsHelper {
-	public static final double TIP_LENGTH = 15;
-	public static final double TIP_ANGLE  = Math.PI / 4;
-	
-	
-	public static void drawArrowTip(Graphics g, Vector2D point, Vector2D direction) {
-		Vector2D tip1 = direction.rotate(TIP_ANGLE).scale(TIP_LENGTH).negate();
-		Vector2D tip2 = direction.rotate(2 * Math.PI - TIP_ANGLE).scale(TIP_LENGTH).negate();
-		drawLine(g, point, tip1);
-		drawLine(g, point, tip2);
-	}
-	
-	public static void drawLine(Graphics g, Vector2D point, Vector2D direction) {
-		g.drawLine((int)point.x, (int)point.y, (int)(point.x + direction.x), (int)(point.y + direction.y));
-	}
+    public static final double TIP_LENGTH = 15;
+    public static final double TIP_ANGLE = Math.PI / 4;
+
+    public static void drawArrowTip(Graphics g, Vector2D point, Vector2D direction) {
+        Vector2D tip1 = direction.rotate(TIP_ANGLE).scale(TIP_LENGTH).negate();
+        Vector2D tip2 = direction.rotate(2 * Math.PI - TIP_ANGLE).scale(TIP_LENGTH).negate();
+        drawLine(g, point, tip1);
+        drawLine(g, point, tip2);
+    }
+
+    public static void drawLine(Graphics g, Vector2D point, Vector2D direction) {
+        g.drawLine((int) point.x, (int) point.y, (int) (point.x + direction.x), (int) (point.y + direction.y));
+    }
 }
