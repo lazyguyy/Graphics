367c32b6d814991babb2595683ffdfe9630c4c14
slyphiX <slyphiX@users.noreply.github.com>
2017-03-03 02:13:25 +0100

    advancing steadily

--- graph/GraphFactory.java
+++ graph/GraphFactory.java
@@ -10,12 +10,14 @@ public abstract class GraphFactory {
     public static Graph createMatrixGraph(GraphIterable itr) {
         return ConstructibleGraph.fromIterable(itr, MatrixGraph::new);
     }
-    //public static Graph createListGraph(GraphIterable itr) {
-    //    return ConstructibleGraph.fromIterable(itr, ListGraph::new);
-    //}
+    public static Graph createListGraph(GraphIterable itr) {
+        return ConstructibleGraph.fromIterable(itr, ListGraph::new);
+    }
 
+    // todo rename dis
     private static abstract class ConstructibleGraph implements Graph {
 
+        // "ist gut nein?" - Uter from "The Simpsons"
         public static <G extends ConstructibleGraph> ConstructibleGraph
                     fromIterable(GraphIterable itr, Function<GraphIterable, G> constructor) {
             return constructor.apply(itr).addEdges(itr);
@@ -34,6 +36,8 @@ public abstract class GraphFactory {
         public Iterator<WeightedEdge> iterator() {
             return new GraphIterator();
         }
+        
+        // TODO fix dis shit immediately
         private class GraphIterator implements Iterator<WeightedEdge> {
 
             private Iterator<WeightedEdge> itr;
@@ -65,9 +69,102 @@ public abstract class GraphFactory {
         }
     }
 
-    //private static class ListGraph extends ConstructibleGraph {
-    //
-    //}
+    private static class ListGraph extends ConstructibleGraph {
+
+        // TODO move vertexnames to ConstructibleGraph
+        private final String[] vertexNames;
+        private final Node[] graph;
+
+        private ListGraph(GraphIterable itr) {
+            int n = itr.vertexCount();
+            vertexNames = itr.vertexNames();
+            graph = new Node[n];
+        }
+
+        @Override
+        public void addEdge(int from, int to, double weight) {
+            graph[from] = new Node(graph[from], to, weight);
+        }
+
+        private Node find(int from, int to) {
+            Node itr = graph[from];
+            while (itr != null) {
+                if (itr.to == to)
+                    return itr;
+                itr = itr.next;
+            }
+            return null;
+        }
+
+        @Override
+        public boolean hasEdge(int from, int to) {
+            return find(from, to) != null;
+        }
+
+        @Override
+        public WeightedEdge edge(int from, int to) {
+            Node n = find(from, to);
+            return new WeightedEdge(from, to, n == null ? Double.POSITIVE_INFINITY : n.weight);
+        }
+
+        @Override
+        public Iterable<WeightedEdge> adjacentEdges(int from) {
+            return new AdjacencyIterator(from);
+        }
+
+        // TODO move this to ConstructibleGraph using delegates for hasNext and next
+        private class AdjacencyIterator implements Iterable<WeightedEdge>, Iterator<WeightedEdge> {
+
+            private Node itr;
+            private final int from;
+
+            public AdjacencyIterator(int from) {
+                this.from = from;
+                this.itr = graph[from];
+            }
+
+            @Override
+            public Iterator<WeightedEdge> iterator() {
+                return this;
+            }
+
+            @Override
+            public boolean hasNext() {
+                return itr != null;
+            }
+
+            @Override
+            public WeightedEdge next() {
+                if (!hasNext())
+                    throw new NoSuchElementException("No more elements");
+                WeightedEdge edge = new WeightedEdge(from, itr.to, itr.weight);
+                itr = itr.next;
+                return edge;
+            }
+        }
+
+        @Override
+        public String[] vertexNames() {
+            return vertexNames;
+        }
+
+        @Override
+        public int vertexCount() {
+            return vertexNames.length;
+        }
+
+        public static class Node {
+            public final Node next;
+            public final int to;
+            public final double weight;
+
+            public Node(Node next, int to, double weight) {
+                this.next = next;
+                this.to = to;
+                this.weight = weight;
+            }
+        }
+    }
 
     private static class MatrixGraph extends ConstructibleGraph {
 
@@ -144,7 +241,6 @@ public abstract class GraphFactory {
         public void addEdge(int from, int to, double weight) {
             graph[from][to] = weight;
         }
-
 
         @Override
         public String toString() {
