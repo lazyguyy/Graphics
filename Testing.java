24a24dd53d4e514734c6f4323b8ba530bb63bcf1
slyphiX <slyphiX@users.noreply.github.com>
2017-03-05 22:12:14 +0100

    added bellman-ford algorithm for testing purposes

--- Testing.java
+++ Testing.java
@@ -1,8 +1,10 @@
 
+import algorithm.ShortestPaths;
 import graph.*;
 import graphgui.*;
 import java.io.IOException;
 import java.io.StringReader;
+import java.util.Arrays;
 import java.util.Locale;
 
 public class Testing {
@@ -10,8 +12,12 @@ public class Testing {
     public static void main(String[] args) throws IOException {
         String source = "A B\nA B -2\nB A -5";
         String source2 = "A B C D E\nA B 5\nA C 10\nB D 1\nD E 3\nB E 7\nA D 40\nB C 20";
+        Graph graph = GraphFactory.createListGraph(GraphIO.read(new StringReader(source2)));
+
         Locale.setDefault(Locale.US);
-        System.out.println(GraphFactory.createMatrixGraph(GraphIO.read(new StringReader(source))));
-        new GraphFrame(GraphFactory.createListGraph(GraphIO.read(new StringReader(source2))));
+        System.out.println(GraphFactory.stringify(graph));
+        // new GraphFrame(graph);
+
+        System.out.println(Arrays.toString(ShortestPaths.BellmanFord(graph, 0)));
     }
 }
